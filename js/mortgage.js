47c9fe7940768442d5698af8cac7fcd8fba3c0f8
anandtoshniwal93 <anandtoshniwal@hotmail.com>
2016-02-24 19:43:28 +0530

    Update mortgage.js

--- js/mortgage.js
+++ js/mortgage.js
@@ -1,38 +1,55 @@
-$(document).ready(function(){
+function interest() {
+    var choice = $("input[name=option]:checked").val();
+    var period_choice = $("input[name=type]:checked").val();
+    var p = $('#principle').val();
+    var n = $('#period').val();
+    var r = $('#rate').val() / 100;
+    if (choice == "ci") {
 
-    $('#dob').datepicker();
-    $('#errormsg').hide();
 
+        if (period_choice == "weekly") {
+             var ans=p*Math.pow(1 +(r/52),n/25);
+            document.getElementById('result').innerText = ans.toFixed(2);
+        } else if (period_choice == "monthly") {
+          var ans=p*Math.pow(1 +(r/12),n/12);
+            document.getElementById('result').innerText = ans.toFixed(2);
+        } else if (period_choice == "yearly") {
+      
+           var a = p*Math.pow((1 + r), n);
+
+            document.getElementById('result').innerText = a.toFixed(2);
+        }
+        document.getElementById('resultl').innerText = "Interest";
+    } else {
+
+
+
+        if (period_choice == "weekly") {
+
+            r = r / 52;
+            n = n * 52;
+
+        } else if (period_choice == "monthly") {
+            r = r / 12;
+            n = n * 12;
+        }
+
+
+        var total = Math.round((p * r * Math.pow((1 + r), n) / [Math.pow((1 + r), n) - 1]));
+        document.getElementById('resultl').innerText = "EMI    ";
+        document.getElementById('result').innerText = total;
+    }
+
+}
+
+$(document).ready(function() {
+    $('.h').hide();
+    $('#radio2').on("change", function(event) {
+        $('.h').show();
+        document.getElementById('result').innerText = "";
+    });
+    $('#radio1').on("change", function(event) {
+        $('.h').hide();
+        document.getElementById('result').innerText = "";
+    });
 });
-function agecalulator()
-{
-  var dtRegex = new RegExp(/\b\d{1,2}[\/-]\d{1,2}[\/-]\d{4}\b/);
-  if(!$('#dob').val()=="" && dtRegex.test($('#dob').val()))
-  {
-
-
-   var dob=new Date($('#dob').val());
-   var dob_month=dob.getMonth();
-   var today=new Date();
-   var days=((today.getFullYear() - dob.getFullYear())*365.25);
-      days += ((today.getMonth()- dob.getMonth())*30.44);
-      days +=(today.getDate() - dob.getDate());
-  document.getElementById('days').innerText =Math.ceil(days);
-  document.getElementById('hours').innerText=today.getHours();
-
-   document.getElementById('min').innerText=today.getMinutes();
- }
- else
- {
-     document.getElementById('errormsg').innerText="Enter Date of birth first or check date format(mm-dd-yyyy)";
-     $('#errormsg').show();
- }
-}
-function reset()
-{
-    $('#errormsg').hide();
-  $('#dob').val('');
-  document.getElementById('days').innerText ="";
-  document.getElementById('hours').innerText="";
-   document.getElementById('min').innerText="";
-}
